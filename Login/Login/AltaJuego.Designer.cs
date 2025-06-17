3604e7c01955421db691f0d858ad61ccb52935f6
Maxi <machilux_08@hotmail.com>
2025-06-17 18:53:17 -0300

    Agrego formulario AltaJuego completo

---
+++ Login/Login/AltaJuego.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace Login
+{
+    partial class AltaJuego
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.cmbDesarrolladora = new System.Windows.Forms.ComboBox();
+            this.cmbClasificacion = new System.Windows.Forms.ComboBox();
+            this.dtpFechaLanzamiento = new System.Windows.Forms.DateTimePicker();
+            this.txtTamanio = new System.Windows.Forms.TextBox();
+            this.cmbFormato = new System.Windows.Forms.ComboBox();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.txtJugadores = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.lblDesarrolladora = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblClasificacion = new System.Windows.Forms.Label();
+            this.lblFormato = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblTamanio = new System.Windows.Forms.Label();
+            this.lblJugadores = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblStock = new System.Windows.Forms.Label();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(71, 48);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(200, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(68, 32);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(93, 13);
+            this.lblNombre.TabIndex = 20;
+            this.lblNombre.Text = "Nombre del juego:";
+            // 
+            // cmbDesarrolladora
+            // 
+            this.cmbDesarrolladora.FormattingEnabled = true;
+            this.cmbDesarrolladora.Location = new System.Drawing.Point(71, 96);
+            this.cmbDesarrolladora.Name = "cmbDesarrolladora";
+            this.cmbDesarrolladora.Size = new System.Drawing.Size(200, 21);
+            this.cmbDesarrolladora.TabIndex = 1;
+            // 
+            // cmbClasificacion
+            // 
+            this.cmbClasificacion.FormattingEnabled = true;
+            this.cmbClasificacion.Location = new System.Drawing.Point(71, 200);
+            this.cmbClasificacion.Name = "cmbClasificacion";
+            this.cmbClasificacion.Size = new System.Drawing.Size(200, 21);
+            this.cmbClasificacion.TabIndex = 3;
+            // 
+            // dtpFechaLanzamiento
+            // 
+            this.dtpFechaLanzamiento.Location = new System.Drawing.Point(71, 257);
+            this.dtpFechaLanzamiento.Name = "dtpFechaLanzamiento";
+            this.dtpFechaLanzamiento.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaLanzamiento.TabIndex = 5;
+            // 
+            // txtTamanio
+            // 
+            this.txtTamanio.Location = new System.Drawing.Point(385, 48);
+            this.txtTamanio.Name = "txtTamanio";
+            this.txtTamanio.Size = new System.Drawing.Size(100, 20);
+            this.txtTamanio.TabIndex = 6;
+            // 
+            // cmbFormato
+            // 
+            this.cmbFormato.FormattingEnabled = true;
+            this.cmbFormato.Location = new System.Drawing.Point(71, 302);
+            this.cmbFormato.Name = "cmbFormato";
+            this.cmbFormato.Size = new System.Drawing.Size(200, 21);
+            this.cmbFormato.TabIndex = 4;
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Location = new System.Drawing.Point(71, 152);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(200, 21);
+            this.cmbCategoria.TabIndex = 2;
+            // 
+            // txtJugadores
+            // 
+            this.txtJugadores.Location = new System.Drawing.Point(385, 97);
+            this.txtJugadores.Name = "txtJugadores";
+            this.txtJugadores.Size = new System.Drawing.Size(100, 20);
+            this.txtJugadores.TabIndex = 7;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(381, 260);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(200, 20);
+            this.txtDescripcion.TabIndex = 8;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(385, 200);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 9;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(222, 372);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 10;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // lblDesarrolladora
+            // 
+            this.lblDesarrolladora.AutoSize = true;
+            this.lblDesarrolladora.Location = new System.Drawing.Point(68, 79);
+            this.lblDesarrolladora.Name = "lblDesarrolladora";
+            this.lblDesarrolladora.Size = new System.Drawing.Size(78, 13);
+            this.lblDesarrolladora.TabIndex = 19;
+            this.lblDesarrolladora.Text = "Desarrolladora:";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(68, 136);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(57, 13);
+            this.lblCategoria.TabIndex = 18;
+            this.lblCategoria.Text = "Categoría:";
+            // 
+            // lblClasificacion
+            // 
+            this.lblClasificacion.AutoSize = true;
+            this.lblClasificacion.Location = new System.Drawing.Point(68, 184);
+            this.lblClasificacion.Name = "lblClasificacion";
+            this.lblClasificacion.Size = new System.Drawing.Size(97, 13);
+            this.lblClasificacion.TabIndex = 17;
+            this.lblClasificacion.Text = "Clasificación Edad:";
+            // 
+            // lblFormato
+            // 
+            this.lblFormato.AutoSize = true;
+            this.lblFormato.Location = new System.Drawing.Point(68, 282);
+            this.lblFormato.Name = "lblFormato";
+            this.lblFormato.Size = new System.Drawing.Size(48, 13);
+            this.lblFormato.TabIndex = 16;
+            this.lblFormato.Text = "Formato:";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(68, 241);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(114, 13);
+            this.lblFecha.TabIndex = 15;
+            this.lblFecha.Text = "Fecha de lanzamiento:";
+            // 
+            // lblTamanio
+            // 
+            this.lblTamanio.AutoSize = true;
+            this.lblTamanio.Location = new System.Drawing.Point(382, 32);
+            this.lblTamanio.Name = "lblTamanio";
+            this.lblTamanio.Size = new System.Drawing.Size(74, 13);
+            this.lblTamanio.TabIndex = 14;
+            this.lblTamanio.Text = "Tamaño (MB):";
+            // 
+            // lblJugadores
+            // 
+            this.lblJugadores.AutoSize = true;
+            this.lblJugadores.Location = new System.Drawing.Point(382, 79);
+            this.lblJugadores.Name = "lblJugadores";
+            this.lblJugadores.Size = new System.Drawing.Size(116, 13);
+            this.lblJugadores.TabIndex = 13;
+            this.lblJugadores.Text = "Cantidad de jugadores:";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(378, 241);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(66, 13);
+            this.lblDescripcion.TabIndex = 12;
+            this.lblDescripcion.Text = "Descripción:";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(389, 136);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(55, 13);
+            this.lblPrecio.TabIndex = 11;
+            this.lblPrecio.Text = "Precio ($):";
+            // 
+            // lblStock
+            // 
+            this.lblStock.AutoSize = true;
+            this.lblStock.Location = new System.Drawing.Point(382, 184);
+            this.lblStock.Name = "lblStock";
+            this.lblStock.Size = new System.Drawing.Size(35, 13);
+            this.lblStock.TabIndex = 21;
+            this.lblStock.Text = "Stock";
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(385, 153);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(100, 20);
+            this.txtStock.TabIndex = 22;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(342, 372);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 23;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // AltaJuego
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.lblStock);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.lblJugadores);
+            this.Controls.Add(this.txtJugadores);
+            this.Controls.Add(this.lblTamanio);
+            this.Controls.Add(this.txtTamanio);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.dtpFechaLanzamiento);
+            this.Controls.Add(this.lblFormato);
+            this.Controls.Add(this.cmbFormato);
+            this.Controls.Add(this.lblClasificacion);
+            this.Controls.Add(this.cmbClasificacion);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.lblDesarrolladora);
+            this.Controls.Add(this.cmbDesarrolladora);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "AltaJuego";
+            this.Text = "Alta de Juego";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.ComboBox cmbDesarrolladora;
+        private System.Windows.Forms.ComboBox cmbClasificacion;
+        private System.Windows.Forms.DateTimePicker dtpFechaLanzamiento;
+        private System.Windows.Forms.TextBox txtTamanio;
+        private System.Windows.Forms.ComboBox cmbFormato;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.TextBox txtJugadores;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label lblDesarrolladora;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblClasificacion;
+        private System.Windows.Forms.Label lblFormato;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label lblTamanio;
+        private System.Windows.Forms.Label lblJugadores;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblStock;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
---
+++ Login/Login/AltaJuego.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class AltaJuego : Form
+    {
+        string conexionbd = "Data Source = localhost\\SQLEXPRESS; Initial Catalog = TiendaJuegos; Integrated Security = True"; /*Conexion Maxi*/
+        public AltaJuego()
+        {
+            InitializeComponent();
+            CargarDesarrolladores();
+            CargarCategoria();
+            CargarClasificacionEdad();
+            CargarFormatos();
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conexion = new SqlConnection(conexionbd))
+            {
+                SqlCommand com = new SqlCommand("SP_altaJuego", conexion);
+                com.CommandType = CommandType.StoredProcedure;
+
+                // Asigno los parametros y le hago la conversion adecuada de cada tipo
+                com.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                com.Parameters.AddWithValue("@IdDesarrolladoraJ", cmbDesarrolladora.SelectedValue);
+                com.Parameters.AddWithValue("@IdCategoria", cmbCategoria.SelectedValue);
+                com.Parameters.AddWithValue("@IdFormato", cmbFormato.SelectedValue);
+                com.Parameters.AddWithValue("@IdClasificacionEdad", cmbClasificacion.SelectedValue);
+                com.Parameters.AddWithValue("@FechaLanzamiento", dtpFechaLanzamiento.Value);
+
+                // valido y convierto campos numericos
+                int tamanio;
+                int jugadores;
+                decimal precio;
+                int stock;
+
+                if (!int.TryParse(txtTamanio.Text, out tamanio) ||
+                    !int.TryParse(txtJugadores.Text, out jugadores) ||
+                    !decimal.TryParse(txtPrecio.Text, out precio) ||
+                    !int.TryParse(txtStock.Text, out stock))
+                {
+                    MessageBox.Show("Verificá que Tamaño, Cantidad de Jugadores, Precio y Stock sean valores numéricos válidos.");
+                    return;
+                }
+
+                com.Parameters.AddWithValue("@Tamaño", tamanio);
+                com.Parameters.AddWithValue("@CantidadJugadores", jugadores);
+                com.Parameters.AddWithValue("@Descripcion", txtDescripcion.Text);
+                com.Parameters.AddWithValue("@Precio", precio);
+                com.Parameters.AddWithValue("@Stock", stock);
+                try
+                {
+                    conexion.Open();
+                    com.ExecuteNonQuery();
+                    MessageBox.Show("Juego agregado correctamente.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al agregar el juego: " + ex.Message);
+                }
+            }
+
+        }
+        private void CargarDesarrolladores()
+        {
+            SqlDataAdapter daDesarrolladores = new SqlDataAdapter("SELECT IdDesarrolladoraJ, Nombre FROM DesarrolladorasJuegos", conexionbd);
+            DataTable dtDesarrolladores = new DataTable();
+            try
+            {
+                daDesarrolladores.Fill(dtDesarrolladores);
+                cmbDesarrolladora.DataSource = dtDesarrolladores;
+                cmbDesarrolladora.DisplayMember = "Nombre";
+                cmbDesarrolladora.ValueMember = "IdDesarrolladoraJ";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar desarrolladoras: " + ex.Message);
+            }
+        }
+
+        private void CargarCategoria()
+        {
+            SqlDataAdapter daCategorias = new SqlDataAdapter("SELECT IDCategoria, Nombre FROM Categorias", conexionbd);
+            DataTable dtCategorias = new DataTable();
+            try
+            {
+                daCategorias.Fill(dtCategorias);
+                cmbCategoria.DataSource = dtCategorias;
+                cmbCategoria.DisplayMember = "Nombre";
+                cmbCategoria.ValueMember = "IDCategoria";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar Categorías: " + ex.Message);
+            }
+        }
+
+
+        private void CargarClasificacionEdad()
+        {
+            SqlDataAdapter daClasificacionEdad = new SqlDataAdapter("SELECT IdClasificacionEdad, Nombre FROM ClasificacionEdades", conexionbd);
+            DataTable dtClasificacionEdad = new DataTable();
+            try
+            {
+                daClasificacionEdad.Fill(dtClasificacionEdad);
+                cmbClasificacion.DataSource = dtClasificacionEdad;
+                cmbClasificacion.DisplayMember = "Nombre";
+                cmbClasificacion.ValueMember = "IdClasificacionEdad";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar Clasificación de Edad: " + ex.Message);
+            }
+        }
+
+
+        private void CargarFormatos()
+        {
+            SqlDataAdapter daFormatos = new SqlDataAdapter("SELECT IdFormato, Nombre FROM Formatos", conexionbd);
+            DataTable dtFormatos = new DataTable();
+            try
+            {
+                daFormatos.Fill(dtFormatos);
+                cmbFormato.DataSource = dtFormatos;
+                cmbFormato.DisplayMember = "Nombre";
+                cmbFormato.ValueMember = "IdFormato";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar Formatos: " + ex.Message);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
