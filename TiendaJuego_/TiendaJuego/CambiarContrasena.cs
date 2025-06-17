6c818add7664b8588191af5d5b55681cfc36de9a
Majo <illanes.mariajose@gmail.com>
2025-06-17 15:36:09 -0300

    Ordenamiento de la carpeta madre

---
+++ TiendaJuego_/TiendaJuego/CambiarContrasena.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class CambiarContrasena : Form
+    {
+        public CambiarContrasena()
+        {
+            InitializeComponent();
+        }
+
+        private void CambiarContrasena_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            Inicio formulario = new Inicio();
+            formulario.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ TiendaJuego_/TiendaJuego/Inicio.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace Login
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-806U76I\SQLEXPRESS;Initial Catalog=TiendaJuegos;Integrated Security=True");
+
+
+        private void btningresar_Click(object sender, EventArgs e)
+        {
+            string usuario = txtusuario.Text;
+            string password = txtcontrasena.Text;
+
+            string connectionString = @"Data Source=DESKTOP-806U76I\SQLEXPRESS;Initial Catalog=TiendaJuegos;Integrated Security=True";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand("VerificarUsuario", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@NombreUsuario", usuario);
+                    cmd.Parameters.AddWithValue("@Contrasena", password);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        bool esAdmin = Convert.ToBoolean(reader["EsAdministrador"]);
+
+                        if (esAdmin)
+                        {
+                            VisorAdmin formulario = new VisorAdmin();
+                            formulario.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            VisorUsuario formulario = new VisorUsuario();
+                            formulario.Show();
+                            this.Hide();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario o contraseña incorrectos");
+                    }
+
+                    reader.Close();
+                }
+            }
+        }
+
+
+
+
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txtusuario.Text = "";
+           txtcontrasena.Text = "";
+                
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LogoTiendaJuego_Click(object sender, EventArgs e)
+        {
+            LogoTiendaJuego.SizeMode = PictureBoxSizeMode.StretchImage;
+           
+        }
+
+        private void OlvidoContrasena_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            CambiarContrasena formulario = new CambiarContrasena();
+            formulario.Show();
+            this.Hide();
+        }
+
+        private void CrearUsuario_Click(object sender, EventArgs e)
+        {
+            CrearUsuario formulario = new CrearUsuario();
+            formulario.Show();
+        }
+    }
+}
