29ef14397d135a05c15518756a586bfaa172f23b
maxiifabeiro <80364038+maxiifabeiro@users.noreply.github.com>
2025-06-20 14:43:54 -0300

    Create Vistas.sql
    Agrego las vistas en archivo vistas

---
+++ Vistas.sql
@@ -0,0 +1,9 @@
+--Vista Desarrolladoras juegos
+
+CREATE VIEW desarrolladorasJUEGOS AS
+SELECT IDDesarrolladoraJ,Nombre,IDPais, FechaFundacion FROM DesarrolladorasJuegos
+
+--Vista Desarrolladoras Consolas
+
+CREATE VIEW desarrolladorasCONSOLAS AS
+SELECT IDDesarrolladorC,Nombre,IDPais, FechaFundacion FROM DesarrolladorasConsola
