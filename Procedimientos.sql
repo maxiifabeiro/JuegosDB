5a01c13c47f46d37b1bf2b56e3ac3bac56b15623
maxiifabeiro <80364038+maxiifabeiro@users.noreply.github.com>
2025-06-20 14:43:18 -0300

    Update Procedimientos.sql
    saco las vistas de este archivo

--- Procedimientos.sql
+++ Procedimientos.sql
@@ -333,12 +333,4 @@ BEGIN
     WHERE NombreUsuario = @NombreUsuario AND Contrasena = @Contrasena
 END
 
---Vista Desarrolladoras juegos
 
-CREATE VIEW desarrolladorasJUEGOS AS
-SELECT IDDesarrolladoraJ,Nombre,IDPais, FechaFundacion FROM DesarrolladorasJuegos
-
---Vista Desarrolladoras Consolas
-
-CREATE VIEW desarrolladorasCONSOLAS AS
-SELECT IDDesarrolladorC,Nombre,IDPais, FechaFundacion FROM DesarrolladorasConsola
